168dddcf5d075ec6c3f4b15d65e4bfd62832ae94
Mirzagayev Aydin <am180@hdm-stuttgart.de>
2020-01-29 16:00:46 +0100

    richtexteditor >quill< integriert

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-$(document).foundation()
+$(document).foundation();
+
+var toolbarOptions = [
+	[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+	[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+	['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+	[{ 'align': [] }],
+
+	[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+	[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+
+	[{ 'font': [] }],
+	['code-block'],
+
+	['clean']                                         // remove formatting button
+];
+
+var quill = new Quill('#editor', {
+	modules: {
+		toolbar: toolbarOptions
+	},
+	theme: 'snow'
+});
+
+$(".folientabelle").on("click", "#duplicateslide", function() {
+	let current_TR = $(this).closest("tr").clone();
+	$(".folientabelle tbody").append(current_TR);
+	return false;
+});
